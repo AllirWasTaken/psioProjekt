670c5a75c3e6e69ef3618312263181603253286d
Lyulf <patryk.chodorowski0@gmail.com>
2024-01-16 09:58:14 +0100

    Remove unused files

--- Experimental/src/SocketErrors.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <stdexcept>
-
-class InvalidSocketError : public std::runtime_error
-{
-public:
-    InvalidSocketError() : std::runtime_error("Client unsafely disconnected") {}
-};
